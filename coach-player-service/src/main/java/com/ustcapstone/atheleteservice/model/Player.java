8e479c3633cff1ba9738810f251ab0b4c24eead0
Aleenaj33 <haleenak33@gmail.com>
2024-11-05 09:40:48 +0530

    Added TeamService to local systme

--- coach-player-service/src/main/java/com/ustcapstone/atheleteservice/model/Player.java
+++ coach-player-service/src/main/java/com/ustcapstone/atheleteservice/model/Player.java
@@ -2,9 +2,10 @@ package com.ustcapstone.atheleteservice.model;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
